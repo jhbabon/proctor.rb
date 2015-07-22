d82c9d81ff95e40210acc51d35e38a7c6d686d40
Juan Hernández <juan.hernandez.babon@gmail.com>
2015-07-22 20:18:01 +0200

    Remove *_path helpers.
    Use the standard way in Sinatra to declare routes.

--- proctor.rb
+++ proctor.rb
@@ -4,6 +4,20 @@ require "sinatra"
 require "sinatra/activerecord"
 require "oj"
 
+require "models"
+require "ability"
+
+use Rack::Auth::Basic do |username, password|
+  user = User.find_by(:name => username)
+  # Use default ENV for main admin user
+  user ||= User.new(
+    :name     => ENV["PROCTOR_ADMIN_USERNAME"],
+    :password => ENV["PROCTOR_ADMIN_PASSWORD"],
+    :role     => "admin"
+  )
+  user && user.authenticate(password)
+end
+
 configure do
   set :database, ENV["PROCTOR_DATABASE_URL"]
 
@@ -24,9 +38,6 @@ configure do
     end
   end
 end
-
-require "models"
-require "ability"
 
 helpers do
 
@@ -68,72 +79,17 @@ helpers do
   end
 end
 
-def users_path
-  "/users"
-end
-
-def user_path(name = ":name")
-  join_paths users_path, name
-end
-
-def user_pubkeys_path(user = ":name")
-  join_paths user_path(user), "pubkeys"
-end
-
-def user_pubkey_path(user = ":name", title = ":title")
-  join_paths user_pubkeys_path(user), title
-end
-
-def user_teams_path(user = ":name")
-  join_paths user_path(user), "teams"
-end
-
-def memberships_path
-  "/memberships"
-end
-
-def teams_path
-  "/teams"
-end
-
-def team_path(name = ":name")
-  join_paths teams_path, name
-end
-
-def team_users_path(name = ":name")
-  join_paths team_path(name), "users"
-end
-
-def team_pubkeys_path(name = ":name")
-  join_paths team_path(name), "pubkeys"
-end
-
-def join_paths(*paths)
-  paths.join("/")
-end
-
-use Rack::Auth::Basic do |username, password|
-  user = User.find_by(:name => username)
-  # Use default ENV for main admin user
-  user ||= User.new(
-    :name     => ENV["PROCTOR_ADMIN_USERNAME"],
-    :password => ENV["PROCTOR_ADMIN_PASSWORD"],
-    :role     => "admin"
-  )
-  user && user.authenticate(password)
-end
-
-before user_path(":name*") do
+before "/users/:name*" do
   @user = User.find_by(:name => params["name"])
   halt 404 if @user.nil?
 end
 
-before user_pubkey_path(":name", ":title*") do
+before "/users/:name/pubkeys/:title*" do
   @pubkey = @user.pubkeys.find_by(:title => params["title"])
   halt 404 if @pubkey.nil?
 end
 
-before team_path(":name*") do
+before "/teams/:name*" do
   @team = Team.find_by(:name => params["name"])
   halt 404 if @team.nil?
 end
@@ -142,21 +98,21 @@ get "/" do
   "Hello world!"
 end
 
-get users_path do
+get "/users" do
   json User.order(:name).map(&:as_api)
 end
 
-get user_path do
+get "/users/:name" do
   json @user.as_api
 end
 
-post users_path, :auth => :admin do
+post "/users", :auth => :admin do
   user = User.new
   user.from_api(parse_body)
 
   if user.save
     status 201
-    location to(user_path(user.name))
+    location to("/users/#{user.name}")
 
     json user.as_api
   else
@@ -166,11 +122,11 @@ post users_path, :auth => :admin do
   end
 end
 
-patch user_path, :auth => %i(admin user), :ability => :@user do
+patch "/users/:name", :auth => %i(admin user), :ability => :@user do
   @user.from_api(parse_body)
 
   if @user.save
-    location to(user_path(@user.name))
+    location to("/users/#{@user.name}")
 
     json @user.as_api
   else
@@ -180,27 +136,27 @@ patch user_path, :auth => %i(admin user), :ability => :@user do
   end
 end
 
-delete user_path, :auth => %i(admin user), :ability => :@user do
+delete "/users/:name", :auth => %i(admin user), :ability => :@user do
   @user.destroy
 
   status 204
 end
 
-get user_pubkeys_path do
+get "/users/:name/pubkeys" do
   json @user.pubkeys.order(:title).map(&:as_api)
 end
 
-get user_pubkey_path do
+get "/users/:name/pubkeys/:title" do
   json @pubkey.as_api
 end
 
-post user_pubkeys_path, :auth => %i(admin user), :ability => :@user do
+post "/users/:name/pubkeys", :auth => %i(admin user), :ability => :@user do
   pubkey = @user.pubkeys.new
   pubkey.from_api(parse_body)
 
   if pubkey.save
     status 201
-    location to(user_pubkey_path(@user.name, pubkey.title))
+    location to("/users/#{@user.name}/pubkeys/#{pubkey.title}")
 
     json pubkey.as_api
   else
@@ -210,11 +166,11 @@ post user_pubkeys_path, :auth => %i(admin user), :ability => :@user do
   end
 end
 
-patch user_pubkey_path, :auth => %i(admin user), :ability => :@pubkey do
+patch "/users/:name/pubkeys/:title", :auth => %i(admin user), :ability => :@pubkey do
   @pubkey.from_api(parse_body)
 
   if @pubkey.save
-    location to(user_pubkey_path(@user.name, @pubkey.title))
+    location to("/users/#{@user.name}/pubkeys/#{@pubkey.title}")
 
     json @pubkey.as_api
   else
@@ -224,29 +180,29 @@ patch user_pubkey_path, :auth => %i(admin user), :ability => :@pubkey do
   end
 end
 
-delete user_pubkey_path, :auth => %i(admin user), :ability => :@pubkey do
+delete "/users/:name/pubkeys/:title", :auth => %i(admin user), :ability => :@pubkey do
   @pubkey.destroy
 
   status 204
 end
 
-get user_teams_path do
+get "/users/:name/teams" do
   json @user.teams.map { |team| team.as_json(only: :name) }
 end
 
-get teams_path do
+get "/teams" do
   json Team.all.map(&:as_api)
 end
 
-get team_path do
+get "/teams/:name" do
   json @team.as_api
 end
 
-patch team_path, :auth => :admin do
+patch "/teams/:name", :auth => :admin do
   @team.attributes = parse_body
 
   if @team.save
-    location to(team_path(@team.name))
+    location to("/teams/#{@team.name}")
 
     json @team.as_api
   else
@@ -256,26 +212,26 @@ patch team_path, :auth => :admin do
   end
 end
 
-delete team_path, :auth => :admin do
+delete "/teams/:name", :auth => :admin do
   @team.destroy
 
   status 204
 end
 
-get team_users_path do
+get "/teams/:name/users" do
   json @team.users.map(&:as_api)
 end
 
-get team_pubkeys_path do
+get "/teams/:name/pubkeys" do
   json @team.pubkeys.map(&:as_api)
 end
 
-post memberships_path, :auth => :admin do
+post "/memberships", :auth => :admin do
   membership = Membership.link(parse_body)
 
   if membership.valid?
     status 201
-    location to(team_path(membership.team.name))
+    location to("/teams/#{membership.team.name}")
   else
     status 422 # TODO: check correct error value
 
@@ -283,7 +239,7 @@ post memberships_path, :auth => :admin do
   end
 end
 
-delete memberships_path, :auth => :admin do
+delete "/memberships", :auth => :admin do
   Membership.unlink(parse_body)
 
   status 204
